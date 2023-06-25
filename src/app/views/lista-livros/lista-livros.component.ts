f0cdf40d28179407d5ae82d457e0886f0879ff29
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-06-25 16:34:35 -0300

    FEAT(lista-livros): Alterando retorno livrosResultadoParaLivros()

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Item } from 'src/app/models/item.model';
+import { LivroVolumeInfo } from 'src/app/models/livro-volume-info';
+import { Livro } from 'src/app/models/livro.model';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -9,18 +12,27 @@ import { LivroService } from 'src/app/service/livro.service';
 })
 export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = ''
   subscription: Subscription;
+  livro: Livro
 
   constructor(private service: LivroService) { }
 
   buscarLivros(campoBusca: string) {
       this.subscription = this.service.buscar(campoBusca).subscribe({
-        next: res => { console.log(res)},
+        next: res => {
+          this.listaLivros = this.livrosResultadoParaLivros(res)
+        },
         error: erro => { console.log(erro)},
         complete: () => console.log("Observable foi completo")
       })
+  }
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
   }
 
   ngOnDestroy(){
