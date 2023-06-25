d8f840b769f94a1061fa1a1e05c7b4d30fddbcc5
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-06-25 16:32:36 -0300

    FEAT(volume-info): Criando interface VolumeInfo()

---
+++ src/app/models/volume-info.model.ts
@@ -0,0 +1,20 @@
+import { ImageLinks } from "./image-links.model";
+
+export interface VolumeInfo {
+    title:               string;
+    authors:             string[];
+    publisher:           string;
+    publishedDate:       string;
+    description:         string;
+    pageCount:           number;
+    printType:           string;
+    mainCategory:        string;
+    categories:          string[];
+    averageRating:       number;
+    ratingsCount:        number;
+    contentVersion:      string;
+    imageLinks:          ImageLinks;
+    language:            string;
+    infoLink:            string;
+    canonicalVolumeLink: string;
+}
