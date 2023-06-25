f2610793e156b06b23157a51477beb8d31b201f2
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-06-25 16:34:06 -0300

    REFACT(livro): Alterando retorno buscar()

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
+import { LivrosResultado } from '../models/volumes-resultado.model';
+import { Item } from '../models/item.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,12 @@ export class LivroService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes'
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<any>{
+  buscar(valorDigitado: string): Observable<Item[]>{
       const params = new HttpParams().append('q', valorDigitado)
-      return this.http.get(this.API, { params })
+      return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+        tap((res) => console.log("Fluxo do tap = ", res)),
+        map((res) => res.items),
+        tap((res) => console.log("Fluxo apos o map = ", res)),
+      )
   }
 }
